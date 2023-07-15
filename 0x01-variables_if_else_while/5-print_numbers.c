e03d690eae6dfbff15be6018f5e6ac71d1fd69ad
oussama elakkoui <mamoto10ichiro@gmail.com>
2023-07-14 19:19:02 -0700

    b

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print numbers from 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - print if the number is postive, zero or negative
+*
+* Description:  main function
+* Return: 0
+*/
+
+int main(void)
+
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+
+	{
+
+		putchar(i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
