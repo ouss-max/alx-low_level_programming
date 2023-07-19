4f3e6d887370e8565f87eb579b6fac8f809f72a9
oussama elakkoui <mamoto10ichiro@gmail.com>
2023-07-18 18:07:30 -0700

    b

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - print putchar
- *
- * Return: Always 0.
+ * Return: always 0
  */
 
-int main(void) {
-  printf("_putchar\n");
-  return 0;
-}
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdioh>
+
+int _putchar(char c)
+
+
+#endif /* MAIN_H */
