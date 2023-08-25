a1cb858417615014cde27793efccb35af9b0ffdc
ouss-max <mamoto10ichiro@gmail.com>
2023-08-25 18:52:05 -0400

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - prints a message before the main function
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+int main(void)
+{
+	printf("This is the main function.\n");
+	return (0);
+}
