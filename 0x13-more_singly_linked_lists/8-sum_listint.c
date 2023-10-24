cd68fa3427a0240bebdc6a0cc0d23db3a921e361
ouss-max <mamoto10ichiro@gmail.com>
2023-10-24 07:48:06 -0400

    sum fix3

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,23 +1,20 @@
 #include "lists.h"
 
 /**
- * sum_listint - Returns the sum of data in a list.
- * @head: The head of the list.
+ * sum_listint - return the sum of data in a list
+ * @head: the head of list.
  *
- * Return: The sum of all the data.
+ * Return: sum of all the data.
  */
 int sum_listint(listint_t *head)
 {
-    if (head == NULL)
-        return (0);
+	int sum=0;
 
-    int sum = 0;
-
-    while (head != NULL)
-    {
-        sum += head->n;
-        head = head->next;
-    }
-
-    return (sum);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
+
